b60995bff8e92b42805530e74c36fe2241bf7104
Frenkie Wang <frenkiewang21@gmail.com>
2025-11-04 06:44:54 +0000

    Change image path

--- frontend/src/inventory.js
+++ frontend/src/inventory.js
@@ -1,48 +1,49 @@
-export const products = [{
-  "pid": 11,
-  "type": "Vegetable",
-  "plant": {
-    "name": "Savoy Cabbage seeds",
-    "price": 3.5
+export const inventory = [{
+    "pid": 11,
+    "type": "Vegetables",
+    "plant": {
+      "name": "Savoy Cabbage seeds",
+      "price": 3.5
+    }
+  }, {
+    "pid": 21,
+    "type": "Fruits",
+    "plant": {
+      "name": "Brambley Apple Tree",
+      "price": 35.0
+    }
+  },
+   {
+    "pid": 211,
+    "type": "Fruits",
+    "plant": {
+      "name": "Conference Pear Tree",
+      "price": 35.0
+    }
+  },
+   {
+    "pid": 121,
+    "type": "Vegetables",
+    "plant": {
+      "name": "Tall Sweetcorn Seeds",
+      "price": 4.5
+    }
+  },
+   {
+    "pid": 88,
+    "type": "Flowers",
+    "plant": {
+      "name": "Dahlia Bubls (10)",
+      "price": 10.00
+    }
+  },
+   {
+    "pid": 188,
+    "type": "Flowers",
+    "plant": {
+      "name": "David Austin Rose",
+      "price": 20.00
+    }
   }
-}, {
-  "pid": 21,
-  "type": "Fruit",
-  "plant": {
-    "name": "Brambley Apple Tree",
-    "price": 35.0
-  }
-},
- {
-  "pid": 211,
-  "type": "Fruit",
-  "plant": {
-    "name": "Conference Pear Tree",
-    "price": 35.0
-  }
-},
- {
-  "pid": 121,
-  "type": "Vegetable",
-  "plant": {
-    "name": "Tall Sweetcorn Seeds",
-    "price": 4.5
-  }
-},
- {
-  "pid": 88,
-  "type": "Flowers",
-  "plant": {
-    "name": "Dahlia Bubls (10)",
-    "price": 10.00
-  }
-},
- {
-  "pid": 188,
-  "type": "Flowers",
-  "plant": {
-    "name": "David Austin Rose",
-    "price": 20.00
-  }
-}
-];
+  ];
+  
